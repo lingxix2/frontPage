3920c18d47e9687453ea27fee5d3b575e346b17c
Lingxi Xu <xulx926@gmail.com>
2020-07-16 11:16:07 +0800

    add fetch

--- src/pages/appCenter/api.js
+++ src/pages/appCenter/api.js
@@ -1,7 +1,6 @@
 import {postFetch} from '@/utils/ajax.js';
 
 
-// const postContentReq = (input={}, callback) => postAjax('/api/v1/application/selectAllIndustrialApp', input, callback);
 const postContentReq = (input={}) => postFetch('/api/v1/application/selectAllIndustrialApp', input);
 
 
--- src/pages/home/api.js
+++ src/pages/home/api.js
@@ -1,12 +1,5 @@
 import {postFetch} from '@/utils/ajax.js';
 
-
-// const postBannerReq = (input={}, callback) => postAjax('/api/v1/content/homePage/carouselMap', input, callback);
-// const postNewsReq = (input={}, callback) => postAjax('/api/v1/consulation/homePage/newsList', input, callback);
-// const postCaseReq = (input={}, callback) => postAjax('/api/v1/content/vendorContent/selectVendorChosenTypicalCases', input, callback);
-
-
-//const postBannerReq = (input={}) => postJSON('/api/v1/content/homePage/carouselMap', input);
 const postBannerReq = (input)=> postFetch('/api/v1/content/homePage/carouselMap', input);
 const postNewsReq = (input={}) => postFetch('/api/v1/consulation/homePage/newsList', input);
 const postCaseReq = (input={}) => postFetch('/api/v1/content/vendorContent/selectVendorChosenTypicalCases', input);
